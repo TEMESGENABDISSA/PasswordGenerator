8348cfa038173ae5b5cf40541b56a1e597a9ae4d
TEMESGENABDISSA <temesgeabdissa2@gmail.com>
2025-01-21 20:17:20 +0300

    Password Generator mini project

--- script.js
+++ script.js
@@ -4,11 +4,22 @@ const upparCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
 const symbol = "@#$%^&*()_+-={}[]|\:;'<>,.?/";
+
+const allchars = upparCase + lowerCase + number + symbol;
+
 function createPassword(){
     let password = "";
     password += upparCase[Math.floor(Math.random () * upparCase.length)];
     password += lowerCase[Math.floor(Math.random () * lowerCase.length)];
     password += number[Math.floor(Math.random    () * number.length)];
     password += symbol[Math.floor(Math.random    () * symbol.length)];
-
+    while(length>password.length){
+        password += allchars[ Math.floor(Math.random()*allchars.length)];
+    }
+    PasswordBox.value = password;
+     
+}
+function copyPassword() {
+    PasswordBox.select();
+    document.execCommand("copy");
 }
